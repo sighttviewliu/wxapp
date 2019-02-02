8f68ae8e82f4edb9fad2fc878b10dc7e634e56e2
sighttviewliu <sighttview@foxmail.com>
2019-02-02 20:13:15 +0800

    title: wxapp base and form component comment: ...

--- myproject/mypages/index/index.js
+++ myproject/mypages/index/index.js
@@ -28,7 +28,82 @@ Page({
   //   { selected: false, imageSource: '../../images/a.jpg' },
   //   { selected: false, imageSource: '../../images/a.jpg' }
   // ]
+
+
+  // sizeList: [10, 20, 30, 40],
+  // typeList: ['success', 'success_no_circle', 'safe_success', 'success_circle', 'info', 'info_circle', 'waiting', 'waiting_circle', 'warn', 'saft_warn', 'circle', 'download', 'cancel', 'search', 'clear'],
+  // colorList: ['green', 'rgb(139,101,8)']
+
+  // content: '内容\n内容\t内容'
+  // percent: 0
+
+  // radios: [
+  //   { value: '1', text: '选项1', checked: false },
+  //   { value: '2', text: '选项2', checked: true },
+  //   { value: '3', text: '选项3', checked: false },
+  //   { value: '4', text: '选项4', checked: false }
+  // ]
+
+// countrys: [
+//   { name: '中', value: '1', checked: false, disabled: false },
+//   { name: '美', value: '2', checked: true, disabled: false },
+//   { name: '日', value: '3', checked: false, disabled: false },
+//   { name: '韩', value: '4', checked: false, disabled: false },
+//   { name: '俄', value: '5', checked: true, disabled: false }
+// ]
+
+// switches: [
+//   {
+//     name: 'switch1',
+//     checked: false,
+//     type: 'switch',
+//     changeEventName: 'change'
+//   },
+//   {
+//     name: 'switch2',
+//     checked: true,
+//     type: 'checkbox',
+//     changeEventName: 'change'
+//   },
+// ]
+
   },
+
+// checkboxchange: function(e) {
+//   console.log(e.detail.value);
+// },
+
+
+// change: function(e) {
+//   var name = e.currentTarget.dataset.name,
+//       switches = this.data.switches,
+//       i,
+//       s;
+
+//   for (i = 0; s = switches[i]; ++i)
+//   {
+//     if (s.name == name)
+//     {
+//       s.checked = e.detail.value;
+//       break;
+//     }
+//   }
+//   console.log(name + '的选中状态为: ' + e.detail.value);
+// }, 
+
+  // checkboxChange: function(e) {
+  //   console.log('选中的选项有: ' + e.detail.value);
+  // },
+
+// changeChoosed: function(e) {
+//   console.log('你选中了: ' + e.detail.value);
+// },
+
+  // click: function() {
+  //   this.setData({
+  //     percent: this.data.percent + 1
+  //   });
+  // },
 
 // switchTab: function(e) {
 //   var _sliderList = this.data.sliderList;
